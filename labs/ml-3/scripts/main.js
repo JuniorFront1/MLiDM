b21e93a9e6e3456e4fd1fdc57451b9e1b4ba7aa1
Nikita <92250677+JuniorFront1@users.noreply.github.com>
2023-05-14 22:35:00 +0400

    Update main.js

--- labs/ml-3/scripts/main.js
+++ labs/ml-3/scripts/main.js
@@ -1,1 +1,48 @@
+/**
+ * Валидация параметров ввода
+ * @param masOfMas
+ */
+function validateElement(mas) {
+    return;
+}
+
+/**
+ * Проверка, являются ли отношения функцией
+ * @param masOfMas
+ */
+function check(mas) {
+    let res = [];
+    let isFunction = true;
+    for (let i = 0; i < mas.length; ++i) {
+        let elem = mas.split(' ');
+        if (res.includes(elem[0]) == 0) {
+            res[elem[0]] = new Set();
+        }
+        res[elem[0]].add(elem[1]);
+        if (res[elem[0]].size > 1) {
+            isFunction = false;
+        }
+    }
+
+    return isFunction;
+}
+
+/**
+ * Основная функция
+ */
+function main() {
+    let mas = document.getElementById('id_mas').value;
+    document.getElementById('result').innerHTML = 'Результат: <br>';
+    mas = mas.split(', ');
+    let IsFunction = true;
+
+    //if (!validateElement(masA, masB, masOfMas)) {
+    //    return;
+    // }
+
+    IsFunction = check(mas);
+    if (!IsFunction)
+        document.getElementById('result').innerHTML += 'Отношения не являются функцией <br>';
+    else document.getElementById('result').innerHTML += 'Отношения являются функцией <br>';
+}
 
